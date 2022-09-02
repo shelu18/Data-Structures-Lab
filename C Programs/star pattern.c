bc9cce5886b39a221cac723a1160064294ba2a6b
Tanay <94126651+patil-tanay@users.noreply.github.com>
2022-09-02 12:58:04 +0530

    Program to print star pattern

---
+++ C Programs/star pattern.c
@@ -0,0 +1,11 @@
+void main(){
+    int j,i,n=5;
+    for (i=1;i<=n;i++){
+        for (j=1;j<=i;j++){
+            printf("*");
+        }
+            {
+                printf("\n");
+            }
+        }
+    }
