dfb8aa3761cae04be31999ee3fe8089819868d15
Tanay <94126651+patil-tanay@users.noreply.github.com>
2022-09-02 12:59:33 +0530

    Create pattern 2.c

---
+++ C Programs/pattern 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*int main() {
+    // Write C code here
+    printf("Hello world");
+
+    return 0;
+}*/
+void main(){
+    int j,i,n=5;
+    for (i=1;i<=n;i++){
+        for (j=1;j<=i;j++){
+            printf("%d",j);
+        }
+            {
+                printf("\n");
+            }
+        }
+    }
