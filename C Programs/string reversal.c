8282c78349a14d8c314eb887f0daad8d83e60707
Tanay <94126651+patil-tanay@users.noreply.github.com>
2022-10-07 12:39:09 +0530

    Reversing string using stak.

---
+++ C Programs/string reversal.c
@@ -0,0 +1,67 @@
+#include <stdio.h>  
+#include <string.h>  
+#define max 100 
+int top,stack[max];  
+  
+void push(char x){  
+  
+     
+      if(top == max-1){  
+          printf("stack overflow");  
+      }  else {  
+          stack[++top]=x;  
+      }  
+  
+}  
+  
+void pop(){  
+    // Pop (Removing element from stack)  
+      printf("%c",stack[top--]);  
+}  
+  
+  
+main()  
+{  
+   char str[]="REVERSED PROFR#include <stdio.h>  
+#include <string.h>  
+#define max 100 
+int top,stack[max];  
+  
+void push(char x){  
+  
+     
+      if(top == max-1){  
+          printf("stack overflow");  
+      }  else {  
+          stack[++top]=x;  
+      }  
+  
+}  
+  
+void pop(){  
+    // Pop (Removing element from stack)  
+      printf("%c",stack[top--]);  
+}  
+  
+  
+main()  
+{  
+   char str[]="REVERSED";  
+   int len = strlen(str);  
+   int i;  
+  
+   for(i=0;i<len;i++)  
+        push(str[i]);  
+  
+   for(i=0;i<len;i++)  
+      pop();  
+";  
+   int len = strlen(str);  
+   int i;  
+  
+   for(i=0;i<len;i++)  
+        push(str[i]);  
+  
+   for(i=0;i<len;i++)  
+      pop();  
+}
